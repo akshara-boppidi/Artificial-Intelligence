d74008c5db647cd202afe308a7fd5b823529a605
akshara boppidi <akshara.boppidi1@marist.edu>
2016-10-14 01:01:21 -0400

    boppidi-03
    hw3

--- HW3/linearregression.py
+++ HW3/linearregression.py
@@ -2,78 +2,41 @@ import numpy as np
 import random
 import os, subprocess
 import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
  
- 
-class Perceptron:
+class linearreg:
     def __init__(self, N):
         # Random linearly separated data
         self.X = self.generate_points(N)
  
     def generate_points(self, N):
-        X, y = self.make_semi_circles(n_samples=N, sep=0.6)
+	    #generates random centers
+        ctrs = 3*np.random.normal(0,1,(2,2))
+		#generates random data following normal distributions
+        X,y=make_blobs(n_samples=100, centers=ctrs, n_features=2,cluster_std=1.0, shuffle=False, random_state=0)
+		#for -1/+1 labels
+        y[ y==0]=-1
+		#plots data
+        c0=plt.scatter(X[y==-1,0],X[y==-1,1],s=20, color='r', marker='x')
+        c1=plt.scatter(X[y==1,0],X[y==1,1],s=20, color='b', marker='o')
+		#displays legend
+        plt.legend((c0,c1),('Class -1','Class +1'),loc='upper right', scatterpoints=1, fontsize=11)
+		#displays axis legends and title
+        plt.xlabel(r'$x_1$')
+        plt.ylabel(r'$x_2$')
+        plt.title(r'Two simple clusters of random data')
+        plt.show()   
         bX = []
-        for k in range(0,N) :
+        for k in range(0,N):
             bX.append((np.concatenate(([1], X[k,:])), y[k]))
-        
+
         # this will calculate linear regression at this point
         X = np.concatenate((np.ones((N,1)), X),axis=1); # adds the 1 constant
         self.linRegW = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)   # lin reg
+        print self.linRegW
         return bX
-
-    def make_semi_circles(self, n_samples=2000, thk=5, rad=10, sep=0.6, plot=True):
-        """Make two semicircles circles
-        A simple toy dataset to visualize classification algorithms.
-        Parameters
-        ----------
-        n_samples : int, optional (default=2000)
-            The total number of points generated.
-        thk : int, optional (default=5)
-            Thickness of the semi circles.
-        rad : int, optional (default=10)
-            Radious of the circle.
-        sep : int, optional (default=5)
-            Separation between circles.
-        plot : boolean, optional (default=True)
-            Whether to plot the data.
-        Returns
-        -------
-        X : array of shape [n_samples, 2]
-            The generated samples.
-        y : array of shape [n_samples]
-            The integer labels (-1 or 1) for class membership of each sample.
-        """
-    
-        noisey = np.random.uniform(low=-thk/100.0, high=thk/100.0, size=(n_samples // 2))
-        
-        noisex = np.random.uniform(low=-rad/100.0, high=rad/100.0, size=(n_samples // 2))
-        
-        separation = np.ones(n_samples // 2)*((-sep*0.1)-0.6)
-        
-        
-        n_samples_out = n_samples // 2
-        n_samples_in = n_samples - n_samples_out
-        
-        # generator = check_random_state(random_state)
-        
-        outer_circ_x = np.cos(np.linspace(0, np.pi, n_samples_out)) + noisex
-        outer_circ_y = np.sin(np.linspace(0, np.pi, n_samples_out)) + noisey
-        inner_circ_x = (1 - np.cos(np.linspace(0, np.pi, n_samples_in))) + noisex
-        inner_circ_y = (1 - np.sin(np.linspace(0, np.pi, n_samples_in)) - .5) + noisey + separation
-        
-        X = np.vstack((np.append(outer_circ_x, inner_circ_x),
-                       np.append(outer_circ_y, inner_circ_y))).T
-        y = np.hstack([np.ones(n_samples_in, dtype=np.intp)*-1,
-                       np.ones(n_samples_out, dtype=np.intp)])
-        
-        if plot:
-            plt.plot(outer_circ_x, outer_circ_y, 'r.')
-            plt.plot(inner_circ_x, inner_circ_y, 'b.')
-            plt.show()
-    
-        return X, y
-    
  
-    def plot(self, mispts=None, vec=None, save=False):
+    def plot(self, mispts=None, vec=None, save=False):        
         fig = plt.figure(figsize=(5,5))
         plt.xlim(-1.5,2.5)
         plt.ylim(-2.0,1.5)
@@ -97,9 +60,8 @@ class Perceptron:
                 plt.title('N = %s with %s test points' \
                           % (str(len(self.X)),str(len(mispts))))
             plt.savefig('p_N%s' % (str(len(self.X))), \
-                        dpi=200, bbox_inches='tight')
+                        dpi=200, bbox_inches='tight')                    
 
- 
     def classification_error(self, vec, pts=None):
         # Error defined as fraction of misclassified points
         if not pts:
@@ -131,7 +93,7 @@ class Perceptron:
         X, N = self.X, len(self.X)
         it = 0
         # Iterate until all points are correctly classified
-        while self.classification_error(w) != 0:
+        while self.classification_error(w) != 0 and it <1000:
             it += 1
             # Pick random misclassified point
             x, s = self.choose_miscl_point(w)
@@ -145,9 +107,10 @@ class Perceptron:
                             dpi=200, bbox_inches='tight')
                 plt.close()
         self.w = w
+        print self.classification_error(self.linRegW)
 
         return it
- 
+
     def check_error(self, M, vec):
         check_pts = self.generate_points(M)
         return self.classification_error(vec, pts=check_pts)
@@ -155,27 +118,8 @@ class Perceptron:
 def main():
     it = np.zeros(1)
     for x in range(0, 1):
-        p = Perceptron(2000)
+        p = linearreg(100)
         it[x] = p.pla(save=False)
         print it
 
-    #n, bins, patches = plt.hist(it, 50, normed=1, facecolor='green', alpha=0.75)
-    #plt.show()
-
-
-    #p.plot()
-
-
 main()
-
-
-
-
-
-
-
-
-
-
-
-
